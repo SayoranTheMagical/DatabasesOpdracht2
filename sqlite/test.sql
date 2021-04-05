7ca59b3e640f5def596d5f4ddcfed7c21a28c4a0
Toan39 <71543094+Toan39@users.noreply.github.com>
2021-04-05 12:56:34 +0200

    gre
    grs

--- sqlite/test.sql
+++ sqlite/test.sql
@@ -17,18 +17,16 @@ CREATE TABLE IF NOT EXISTS Game(
 );
 
 /* inserting data into table Game*/
-INSERT INTO Game  VALUES('G1DQAE01', 'D1DQBF01', 'DSP1DQ01', 'Mario Kart 8', 'SEP1DQ01', 'Mario Kart 8', 'Race');
-INSERT INTO Game  VALUES('G1DQAE02', 'D1DQBF02', 'DSP1DQ02', 'Mario Kart DS', 'SEP1DQ02', 'Mario Kart DS', 'Race');
-INSERT INTO Game  VALUES('G1DQAE03', 'D1DQBF03', 'DSP1DQ03', 'Crysis', 'SEP1DQ01', 'Crysis 3', 'P1DQ01');
+INSERT INTO Game  VALUES('G1DQAE01', 'D1DQBF01', 'DSP1DQ01', 'Mario Kart', 'SEP1DQ01', 'Mario Kart 8', 'Race');
+INSERT INTO Game  VALUES('G1DQAE02', 'D1DQBF02', 'DSP1DQ02', 'Mario Kart', 'SEP1DQ03', 'Mario Kart 7', 'Race');
+INSERT INTO Game  VALUES('G1DQAE03', 'D1DQBF03', 'DSP1DQ03', 'Mario Kart', 'SEP1DQ03', 'Mario Kart 7', 'Race');
 INSERT INTO Game  VALUES('G1DQAE04', 'D1DQBF04', 'DSP1DQ04', 'Game 1', 'SEP1DQ01', 'Game 1', 'P1DQ01');
 INSERT INTO Game  VALUES('G1DQAE05', 'D1DQBF05', 'DSP1DQ05', 'Game 1', 'SEP1DQ01', 'Game 1', 'P1DQ01');
 INSERT INTO Game  VALUES('G1DQAE06', 'D1DQBF06', 'DSP1DQ06', 'Game 1', 'SEP1DQ01', 'Game 1', 'P1DQ01');
-INSERT INTO Game  VALUES('G1DQAE07', 'D1DQBF07', 'DSP1DQ07', 'Crysis 3', 'SEP1DQ01', 'Crysis 3', 'P1DQ01');
+INSERT INTO Game  VALUES('G1DQAE07', 'D1DQBF07', 'DSP1DQ07', 'Crysis series', 'SEP1DQ01', 'Crysis 3', 'Shooter');
 INSERT INTO Game  VALUES('G1DQAE08', 'D1DQBF08', 'DSP1DQ08', 'Game 1', 'SEP1DQ01', 'Game 1', 'P1DQ01');
 INSERT INTO Game  VALUES('G1DQAE09', 'D1DQBF09', 'DSP1DQ09', 'Game 1', 'SEP1DQ01', 'Game 1', 'P1DQ01');
 INSERT INTO Game  VALUES('G1DQAE10', 'D1DQBF10', 'DSP1DQ10', 'Game 1', 'SEP1DQ01', 'Game 1', 'P1DQ01');
-
-
 
 CREATE TABLE IF NOT EXISTS System (
 	`SystemID` varchar(8) NOT NULL, 
@@ -41,17 +39,17 @@ CREATE TABLE IF NOT EXISTS System (
 );
 
 /* inserting data into table System*/
-INSERT INTO Game  VALUES('SEP1DQ01', 'Wii U', 'CSP1DQ01', '18-11-2012', '64');
-INSERT INTO Game  VALUES('SEP1DQ02', 'Wii', 'CSP1DQ01', '19-11-2016', '32');
-INSERT INTO Game  VALUES('SEP1DQ03', 'Nintendo DS', 'CSP1DQ02', '19-11-2016', '32');
-INSERT INTO Game  VALUES('SEP1DQ03', 'Nintendo 3DS', 'CSP1DQ02', '19-11-2016', '32');
-INSERT INTO Game  VALUES('SEP1DQ08', 'Nintendo Switch', 'CSP1DQ02', '19-11-2016', '32');
-INSERT INTO Game  VALUES('SEP1DQ04', 'Playstation 2', 'CSP1DQ03', '19-11-2016', '32');
-INSERT INTO Game  VALUES('SEP1DQ04', 'Playstation 3', 'CSP1DQ03', '19-11-2016', '32');
-INSERT INTO Game  VALUES('SEP1DQ05', 'Playstation 4', 'CSP1DQ03', '19-11-2016', '32');
-INSERT INTO Game  VALUES('SEP1DQ06', 'Xbox 360', 'CSP1DQ04', '19-11-2016', '32');
-INSERT INTO Game  VALUES('SEP1DQ07', 'Xbox one', 'CSP1DQ04', '19-11-2016', '32');
-INSERT INTO Game  VALUES('SEP1DQ08', 'Xbox one X', 'CSP1DQ04', '19-11-2016', '32');
+INSERT INTO Game  VALUES('SEP1DQ01', 'Wii U', 'CSP1DQ01', '18-11-2012', '32');
+INSERT INTO Game  VALUES('SEP1DQ02', 'Wii Black', 'CSP1DQ01', '28-08-2012', '32');
+INSERT INTO Game  VALUES('SEP1DQ03', 'Nintendo 2DS', 'CSP1DQ02', '12-11-2013', '64');
+INSERT INTO Game  VALUES('SEP1DQ04', 'Nintendo 3DS', 'CSP1DQ02', '26-02-2011', '64');
+INSERT INTO Game  VALUES('SEP1DQ05', 'Nintendo Switch', 'CSP1DQ02', '03-03-2017', '64');
+INSERT INTO Game  VALUES('SEP1DQ06', 'Playstation 2', 'CSP1DQ03', '04-03-2000', '64');
+INSERT INTO Game  VALUES('SEP1DQ07', 'Playstation 3', 'CSP1DQ03', '11-11-2006', '64');
+INSERT INTO Game  VALUES('SEP1DQ08', 'Playstation 4', 'CSP1DQ03', '15-11-2013', '64');
+INSERT INTO Game  VALUES('SEP1DQ09', 'Xbox 360', 'CSP1DQ04', '22-11-2005', '64');
+INSERT INTO Game  VALUES('SEP1DQ10', 'Xbox One', 'CSP1DQ04', '22-11-2013', '64');
+INSERT INTO Game  VALUES('SEP1DQ11', 'Xbox One X', 'CSP1DQ04', '10-11-2020', '64');
 
 CREATE TABLE IF NOT EXISTS DevelopmentStudio (
 	`DSID` varchar(8) NOT NULL, 
@@ -95,10 +93,10 @@ CREATE TABLE IF NOT EXISTS ConsoleLine (
 );
 
 /* inserting data into table ConsoleLine*/
-INSERT INTO Game  VALUES('CSP1DQ01', 'Wii', '19-11-2016', '32');
-INSERT INTO Game  VALUES('CSP1DQ02', 'Nintendo', '19-11-2016', '64');
-INSERT INTO Game  VALUES('CSP1DQ03', 'Playstation','19-11-2016', '32');
-INSERT INTO Game  VALUES('CSP1DQ04', 'Xbox', '19-11-2016', '32');
+INSERT INTO Game  VALUES('CSP1DQ01', 'Wii', '19-11-2006', '32');
+INSERT INTO Game  VALUES('CSP1DQ02', 'Nintendo', '21-11-2004', '64');
+INSERT INTO Game  VALUES('CSP1DQ03', 'Playstation','3-12-1994', '64');
+INSERT INTO Game  VALUES('CSP1DQ04', 'Xbox', '15-11-2001', '64');
 
 /* supertype of isa-hierarchy */
 /* uses a composite primary key, since foreign keys of the child tables are also composite keys */
