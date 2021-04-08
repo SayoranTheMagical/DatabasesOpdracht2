bf1e7f1b93191ff29609e61cc62af529cf26f7c7
Toan39 <71543094+Toan39@users.noreply.github.com>
2021-04-08 19:13:51 +0200

    redgh
    rdhe

--- sqlite/q2.sql
+++ sqlite/q2.sql
@@ -1,47 +1,34 @@
 --Query 2
 /* */
 
-SELECT GameData.Title,
+--Default Query A  with WHERE not possible, EXCEPT-query has duplicate rows
+SELECT GameData.Title
+FROM GameData
+EXCEPT
+SELECT GameData.Title
        System.Name
+FROM System
+INNER JOIN Game ON Game.SystemID = System.SystemID
+INNER JOIN GameData ON GameData.Title = Game.Title
+WHERE System.Name != "Playstation 3";
+
+
+--Query B with HAVING
+SELECT GameData.Title,
+        System.Name
 FROM GameData
 INNER JOIN Game ON GameData.Title = Game.Title 
-INNER JOIN System ON System.SystemID = Game.SystemID
+LEFT JOIN System ON System.SystemID = Game.SystemID
 GROUP BY GameData.Title
 HAVING System.Name != "Playstation 3";
 
--- SELECT GameFranchise.FranchiseName,
---         GameData.Genre
--- FROM GameFranchise
--- INNER JOIN SubFranchise ON SubFranchise.FranchiseName = GameFranchise.FranchiseName 
--- INNER JOIN GameData ON GameData.SubFranchiseName = SubFranchise.SubFranchiseName
--- GROUP BY GameFranchise.FranchiseName
--- HAVING GameData.Genre != "Race";
-
--- SELECT GameFranchise.FranchiseName
--- FROM GameFranchise
--- INNER JOIN SubFranchise ON SubFranchise.FranchiseName = GameFranchise.FranchiseName 
--- INNER JOIN GameData ON GameData.SubFranchiseName = SubFranchise.SubFranchiseName
--- GROUP BY GameData.Title
--- HAVING GameData.Genre = "Race";
--- EXCEPT
--- SELECT GameFranchise.FranchiseName
--- FROM GameFranchise
--- INNER JOIN SubFranchise ON SubFranchise.FranchiseName = GameFranchise.FranchiseName 
--- INNER JOIN GameData ON GameData.SubFranchiseName = SubFranchise.SubFranchiseName
--- GROUP BY GameData.Title
--- HAVING GameData.Genre = "Platformer";
-
-
-SELECT GameFranchise.FranchiseName
+SELECT GameFranchise.FranchiseName,
+        GameData.Genre
 FROM GameFranchise
 INNER JOIN SubFranchise ON SubFranchise.FranchiseName = GameFranchise.FranchiseName 
 INNER JOIN GameData ON GameData.SubFranchiseName = SubFranchise.SubFranchiseName
-GROUP BY GameData.Title
-HAVING GameData.Genre != "Race"
-EXCEPT
-SELECT GameFranchise.FranchiseName
-FROM GameFranchise
-INNER JOIN SubFranchise ON SubFranchise.FranchiseName = GameFranchise.FranchiseName 
-INNER JOIN GameData ON GameData.SubFranchiseName = SubFranchise.SubFranchiseName
-GROUP BY GameData.Title
-HAVING GameData.Genre = "Platformer";
+GROUP BY GameFranchise.FranchiseName
+HAVING GameData.Genre != "Race";
+
+
+
--- sqlite/q4.sql
+++ sqlite/q4.sql
@@ -14,3 +14,18 @@ INNER JOIN Subfranchise ON Subfranchise.FranchiseName = GameFranchise.FranchiseN
 LEFT JOIN GameData ON GameData.SubFranchiseName = Subfranchise.SubFranchiseName
 GROUP BY GameFranchise.FranchiseName
 HAVING GameData.Genre != "Race";
+
+
+SELECT GameFranchise.FranchiseName
+FROM GameFranchise
+INNER JOIN SubFranchise ON SubFranchise.FranchiseName = GameFranchise.FranchiseName 
+INNER JOIN GameData ON GameData.SubFranchiseName = SubFranchise.SubFranchiseName
+GROUP BY GameData.Title
+HAVING GameData.Genre != "Race"
+EXCEPT
+SELECT GameFranchise.FranchiseName
+FROM GameFranchise
+INNER JOIN SubFranchise ON SubFranchise.FranchiseName = GameFranchise.FranchiseName 
+INNER JOIN GameData ON GameData.SubFranchiseName = SubFranchise.SubFranchiseName
+GROUP BY GameData.Title
+HAVING GameData.Genre = "Platformer";
