9db24b375ff4db6f3df6a06efe560259acf6d8e6
SayoranTheMagical <74265273+SayoranTheMagical@users.noreply.github.com>
2021-04-10 21:59:36 +0200

    Update q4.sql

--- sqlite/q4.sql
+++ sqlite/q4.sql
@@ -1,5 +1,11 @@
 --Query 4
-/*  */
+/*This query is meant for someone who wants to know what the span of genres is within a given franchise.
+In th query below we assumed that this person wants to find a franchise with platformer- as well as familygames.
+The relevant information to be projected is the FranchiseName, which we therefore select at the beginning.
+After which we check if within said FranchiseName, a game is present which genre is Platformer and make sure to
+except the ones where there is not a Family-game.
+We join the GameFranchise and Subfranchise table with the matching(FranchiseName) and we thereafter we
+join Subfranchise and GameDat with the corresponding Attribute(SubFranchiseName).d*/
 
 SELECT FranchiseName FROM GameFranchise
 WHERE FranchiseName IN(
